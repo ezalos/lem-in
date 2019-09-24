7e74689afc0f190014873a01aecc539c1730d63b
Yohan THOMAS <ythomas@e2r12p11.42.fr>
2019-09-24 18:16:39 +0200

    file normed

--- srcs/creat_position.c
+++ srcs/creat_position.c
@@ -6,7 +6,7 @@
 /*   By: ythomas <ythomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 14:38:39 by ythomas           #+#    #+#             */
-/*   Updated: 2019/09/23 20:14:30 by ezalos           ###   ########.fr       */
+/*   Updated: 2019/09/24 18:15:57 by ythomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,11 @@ int				get_len_actual_path(t_visu *visu, int i)
 
 void			calculate_coord_last_room(t_visu *visu)
 {
-	// int		len;
 	double	rate_y;
 	int		i;
 	int		j;
 
 	rate_y = ((visu->screen_h - 100.0) / (double)visu->nb_h);
-	// len = get_len_actual_path(visu, 0);
 	i = 0;
 	while (visu->paths[i])
 	{
@@ -44,26 +42,6 @@ void			calculate_coord_last_room(t_visu *visu)
 		i++;
 	}
 }
-// void			calculate_coord_last_room(t_visu *visu)
-// {
-// 	int		len;
-// 	double	rate_y;
-// 	int		i;
-// 	int		j;
-
-// 	rate_y = ((visu->screen_h - 100.0) / (double)visu->nb_h);
-// 	len = get_len_actual_path(visu, 0);
-// 	i = 0;
-// 	while (visu->paths[i])
-// 	{
-// 		j = 0;
-// 		while (visu->paths[i][j])
-// 			j++;
-// 		visu->paths[i][j - 1]->y = ((double)visu->nb_h * (rate_y)) + 50.0;
-// 		visu->paths[i][j - 1]->x = ((visu->screen_w - 100.0) / 2.0) + 50.0;
-// 		i++;
-// 	}
-// }
 
 void			calculate_coord(t_visu *visu, int i, int j)
 {
@@ -75,26 +53,7 @@ void			calculate_coord(t_visu *visu, int i, int j)
 	visu->paths[i][j]->y = ((double)j * (rate_y)) + 50.0;
 	visu->paths[i][j]->x = visu->tmpx;
 	visu->paths[i][j]->z = visu->tmpz;
-
 }
-
-// void			calculate_coord(t_visu *visu, int i, int j)
-// {
-// 	int		len;
-// 	double	rate_y;
-
-// 	len = get_len_actual_path(visu, i);
-// 	rate_y = ((visu->screen_h - 100.0) / (double)len);
-// 	visu->paths[i][j]->y = ((double)j * (rate_y)) + 50.0;
-// 	if (i == 0)
-// 		visu->paths[i][j]->x = ((visu->screen_w - 100.0) / 2.0) + 50.0;
-// 	else if (i % 2 != 0)
-// 		visu->paths[i][j]->x = (((visu->screen_w - 100.0) / 2.0)
-// 			+ (visu->range * visu->rate_x)) + 50.0;
-// 	else
-// 		visu->paths[i][j]->x = (((visu->screen_w - 100.0) / 2.0)
-// 			- (visu->range * visu->rate_x)) + 50.0;
-// }
 
 void			calculate_coord_firsts_rooms(t_visu *visu)
 {
@@ -113,20 +72,6 @@ void			calculate_coord_firsts_rooms(t_visu *visu)
 	}
 }
 
-// void			calculate_coord_firsts_rooms(t_visu *visu)
-// {
-// 	int i;
-
-// 	i = 1;
-// 	calculate_coord(visu, 0, 0);
-// 	while (visu->paths[i])
-// 	{
-// 		visu->paths[i][0]->x = visu->paths[0][0]->x;
-// 		visu->paths[i][0]->y = visu->paths[0][0]->y;
-// 		i++;
-// 	}
-// }
-
 void			init_new_coord(t_visu *visu)
 {
 	int i;
@@ -139,8 +84,10 @@ void			init_new_coord(t_visu *visu)
 	while (visu->paths[i])
 	{
 		j = 1;
-		visu->tmpx = visu->centre + visu->rayon * cos((double)i * 2.0 * M_PI / (double)visu->nb_paths);
-		visu->tmpz = visu->centre + visu->rayon * sin((double)i * 2.0 * M_PI / (double)visu->nb_paths);
+		visu->tmpx = visu->centre + visu->rayon * cos(
+			(double)i * 2.0 * M_PI / (double)visu->nb_paths);
+		visu->tmpz = visu->centre + visu->rayon * sin(
+			(double)i * 2.0 * M_PI / (double)visu->nb_paths);
 		while (visu->paths[i][j + 1] != NULL)
 		{
 			calculate_coord(visu, i, j);
